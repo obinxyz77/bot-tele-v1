4c3f41b697d79665b4f55adc2359e9481a2d9a71
obinxyz77 <robinasqi6@gmail.com>
2025-10-29 13:25:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Credit: @ObinXyz | Telegram: t.me/obinexyezet
 // yang ganti credit gw sumpahin scnya eror 
 export const config = {
-  botToken: "8432032707:AAGfgnqSCZdoiKP_JDUg2mDa8XRELnwaCS0", // Ganti token dari @BotFather
+  botToken: "-", // Ganti token dari @BotFather
 };
